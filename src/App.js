4c8613b2b0d0dda40d499608de36bc613285e3f1
Jorge Raze <jorge.alcaraz@ibm.com>
2022-08-18 19:04:31 -0500

    Carousel and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Navigation from "./components/Navigation";
+import CarouselBig from "./components/CarouselBig";
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <CarouselBig />
     </div>
   );
 }
