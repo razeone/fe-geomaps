1e992b3d4a6c605639982ccf0d347ae4f5a4ecd0
Jorge Alberto Alcaraz Reyes <jorgealc@microsoft.com>
2022-11-17 19:11:59 -0600

    Add search bar on countries table

--- src/components/CountriesTable.js
+++ src/components/CountriesTable.js
@@ -1,31 +1,46 @@
-import React from "react";
-import { Table, Button } from "react-bootstrap";
+import { useState, React } from "react";
+import { Table, Button, Container, Row } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 const CountriesTable = ({ countries }) => {
+    // Function to filter the countries based on the search input
+    const [search, setSearch] = useState("");
+    let countriesFullSet = countries;
+    const filteredCountries = countriesFullSet.filter((country) => {
+        return country.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Código de País</th>
-                    <th>Código de Llamada</th>
-                </tr>
-            </thead>
-            <tbody>
-                {countries.map((country) => (
-                    <tr key={country.name}>
-                        <td>
-                            <LinkContainer to={`/country-population/${country.name}`}>
-                                <Button variant="link">{country.name}</Button>
-                            </LinkContainer>
-                        </td>
-                        <td>{country.country_code}</td>
-                        <td>{country.phone_code}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
+        <Container>
+            <Row>
+                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}  placeholder="Filtrar..." />
+            </Row>
+            <br />
+            <Row>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Código de País</th>
+                            <th>Código de Llamada</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredCountries.map((country) => (
+                            <tr key={country.name}>
+                                <td>
+                                    <LinkContainer to={`/country-population/${country.name}`}>
+                                        <Button variant="link">{country.name}</Button>
+                                    </LinkContainer>
+                                </td>
+                                <td>{country.country_code}</td>
+                                <td>{country.phone_code}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
     );
 }
 
